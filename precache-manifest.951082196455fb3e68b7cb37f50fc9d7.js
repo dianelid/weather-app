bb87b8711609827a43cdcb05b3f1354e5ea4fd3e
Lidiane Santos <diane_lid@hotmail.com>
2020-02-17 17:28:31 -0300

    Updates

---
+++ precache-manifest.951082196455fb3e68b7cb37f50fc9d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ee8dfe05116cf9bc5edece588b3bf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0756184247827246a056",
+    "url": "/static/css/main.678684f8.chunk.css"
+  },
+  {
+    "revision": "c9928cdbd18b086f11ed",
+    "url": "/static/js/2.0169fcfd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0169fcfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0756184247827246a056",
+    "url": "/static/js/main.3195e6b6.chunk.js"
+  },
+  {
+    "revision": "89b89fff14a191b19919",
+    "url": "/static/js/runtime-main.1bdcfc2d.js"
+  }
+]);
